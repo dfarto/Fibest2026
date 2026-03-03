e4590992dc23bb8d7aa05c12a3640bd36fe497cc
dfarto <131961342+dfarto@users.noreply.github.com>
2026-03-03 12:06:29 +0100

    Update id_discovery.py

--- taller1/id_discovery.py
+++ taller1/id_discovery.py
@@ -8,25 +8,23 @@ id_res = ""
 
 def capture_response():
     global bus, id_res
-    bus = can.interface.Bus(interface="socketcan", channel="vcan0", bitrate=500000)
     while not id_res:
-        msg = bus.recv(count=1)
-        print(msg.data)
-        if msg and [0x10, 0x50, 0x01] in msg.data:
+        msg = bus.recv(timeout=1.0)
+        if msg and len(list(msg.data))>3 and [0x50, 0x03] == list(msg.data)[1:3]:
             id_res = msg.arbitration_id
             break
 
 msg = can.Message(
-                arbitration_id=0x600,
-                data=[0x02,0x10, 0x01, 0xAA, 0xBB, 0xCC, 0xDD, 0xAA],
-                is_extended_id=False
-            )
+        arbitration_id=0x600,
+        data=[0x02,0x10, 0x03, 0xAA, 0xBB, 0xCC, 0xDD, 0xAA],
+        is_extended_id=False
+    )
 
 basic_msg_thread = threading.Thread(target=capture_response)
 basic_msg_thread.start()
 while not id_res:
     bus.send(msg)
-    sleep(0.15)
+    sleep(0.1)
     if not id_res:
         msg.arbitration_id += 1
     else: 
@@ -34,4 +32,5 @@ while not id_res:
     if msg.arbitration_id >= 0x800:
         break
 
-print(f"los ids son -> req: {msg.arbitration_id}, res: {id_res}")
+id_res = id_res if id_res else 12
+print(f"los ids son -> req: {hex(msg.arbitration_id)}, res: {hex(id_res)}")
