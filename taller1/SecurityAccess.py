6bdee6809871302253b06cd01339c8794683e6ea
dfarto <131961342+dfarto@users.noreply.github.com>
2026-03-03 12:07:31 +0100

    Update and rename id_discovery.py to SecurityAccess.py

---
+++ taller1/SecurityAccess.py
@@ -0,0 +1,45 @@
+import can
+from time import sleep
+
+request_id = 0x6DB
+response_id = 0x6E8
+
+bus = can.interface.Bus(interface="socketcan", channel=channel, bitrate=500000)
+
+#step 1: extended session.
+msg = can.Message(
+                        arbitration_id=request_id,
+                        data=[0x02,0x10, 0x03, 0xAA, 0xBB, 0xCC, 0xDD, 0xAA],
+                        is_extended_id=False
+                    )
+bus.send(msg)
+sleep(0.1)
+
+#step 2: seed request
+msg = can.Message(
+                        arbitration_id=request_id,
+                        data=[0x02,0x27, 0x01, 0xAA, 0xBB, 0xCC, 0xDD, 0xAA],
+                        is_extended_id=False
+                    )
+bus.send(msg)
+while True:
+    response = bus.recv(1)
+    if response and response.arbitration_id == response_id and list(response.data)[1]==0x67:
+        data = list(response.data)[3:]
+        break
+sleep(0.1)
+# Decrypt tries:
+#############################
+
+msg = can.Message(
+    arbitration_id=request_id,
+    data=[0x07,0x27, 0x02] + decrypt,
+    is_extended_id=False
+)
+bus.send(msg)
+
+while True:
+    response = bus.recv(1)
+    if response and response.arbitration_id == response_id:
+        print(response)
+        break
